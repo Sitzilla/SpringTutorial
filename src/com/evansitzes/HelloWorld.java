17fc0461cbd770b25678cd8ace3008acf263ac8f
Evan <evan@datarank.com>
2016-03-28 10:00:09 -0500

    added init/destroy hooks

--- src/com/evansitzes/HelloWorld.java
+++ src/com/evansitzes/HelloWorld.java
@@ -10,4 +10,12 @@ public class HelloWorld {
 	public void getMessage() {
 		System.out.println("Your message: " + message);
 	}
+
+    public void init() {
+        System.out.println("Initializing bean...");
+    }
+
+    public void destroy() {
+        System.out.println("Destroying bean...");
+    }
 }
